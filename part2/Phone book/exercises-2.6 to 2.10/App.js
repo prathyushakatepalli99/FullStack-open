4f535e71e21118983957167e34c0ba94066ecf81
Prathyusha Katepalli <74001138+prathyushakatepalli99@users.noreply.github.com>
2021-06-06 11:17:59 +0530

    Update App.js

--- part2/Phone book/exercises-2.6 to 2.10/App.js
+++ part2/Phone book/exercises-2.6 to 2.10/App.js
@@ -1,36 +1,6 @@
 import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
-
-const Phonebook = [
-	{ 
-		id: 1,
-		name: 'Adam', 
-		number: "91234567809"
-	},
-	{
-		id: 2,
-		name: 'Arto', 
-		number: "45676112347"
-	},
-	{
-		id: 3,
-		name: 'Ma Yutao', 
-		number: "76436573858"
-	},
-	{
-		id: 4,
-		name: 'Dai Daisy', 
-		number: "12345678901"
-	},
-	{
-		id: 5,
-		name: 'Dan Abramoy', 
-		number: "34567890123"
-	}
-]
-
-
 const Filter = (props) => {
 	
 	return (
