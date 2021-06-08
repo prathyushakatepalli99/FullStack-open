223204af1ce07ff503d27d2a5cc8711caec060ed
prathyushakatepalli99 <prathyushakatepalli99@gmail.com>
2021-06-08 19:59:41 +0530

    part6

---
+++ part6/unicafe-redux/src/reducers/counterreducer.js
@@ -0,0 +1,35 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterreducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'DO_NOTHING':
+      return state
+    case 'GOOD':
+      return {
+        ...state,
+        good: state.good + 1
+      }
+    case 'OK':
+      return {
+        ...state,
+        ok: state.ok + 1
+      }
+    case 'BAD':
+      return {
+        ...state,
+        bad: state.bad + 1
+      }
+    case 'ZERO':
+      return initialState
+    default: return state
+  }
+  
+}
+
+export default counterreducer
+
